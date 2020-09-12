f2030014603b5a5fe04671e633db29ab474e82a4
Andrew Nickyforov <nik_andrey@ukr.net>
2020-09-12 17:38:13 +0300

    Updates

---
+++ precache-manifest.b57301c12ba726ed08e79fa00f9128b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331ca7de76aecaec6dcdb58a8bf1d039",
+    "url": "/http//5lum6er.github.io/abz_tt/index.html"
+  },
+  {
+    "revision": "11755df95a247aa5b25b",
+    "url": "/http//5lum6er.github.io/abz_tt/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "6e109e76b5a7107d7c97",
+    "url": "/http//5lum6er.github.io/abz_tt/static/js/2.1f5b9346.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http//5lum6er.github.io/abz_tt/static/js/2.1f5b9346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11755df95a247aa5b25b",
+    "url": "/http//5lum6er.github.io/abz_tt/static/js/main.ba081c5c.chunk.js"
+  },
+  {
+    "revision": "6ef22c54fdf446a3bd1d",
+    "url": "/http//5lum6er.github.io/abz_tt/static/js/runtime-main.b492c2d6.js"
+  }
+]);
